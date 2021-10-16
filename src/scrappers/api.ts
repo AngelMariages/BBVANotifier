972218b18870cf92e61455c4eedae135057fd991
Àngel Mariages <angel.mariages@gmail.com>
2021-10-16 23:47:14 +0200

    always get auth

--- src/scrappers/api.ts
+++ src/scrappers/api.ts
@@ -5,11 +5,23 @@ type BBVAAuth = {
 	customerId: string;
 }
 
+type ViewersFinancialDashboardResponse = {
+	familyBalances?: [{
+		balance: {
+			amount: number;
+		};
+	}]
+};
+
+type GrantingTicketsResponse = {
+	user?: {
+		id: string;
+	}
+}
+
 export class ApiScrapper {
 	private user: string;
 	private password: string;
-
-	private accountReady: Promise<BBVAAuth> | null = null;
 
 	constructor(user: string, password: string) {
 		this.user = user;
@@ -17,7 +29,15 @@ export class ApiScrapper {
 	}
 
 	public async getAssociatedAccountBalance(): Promise<number> {
-		const { authToken, customerId } = await this.getAuth();
+		const auth = await this.getAuth();
+
+		if (!auth) {
+			console.log(`No auth for user ${this.user}`);
+
+			return 0.0;
+		}
+
+		const { authToken, customerId } = auth;
 
 		return await this.getBalance(authToken, customerId);
 	}
@@ -35,54 +55,53 @@ export class ApiScrapper {
 			}
 		);
 
-		const acc = await resp.json();
+		const acc = await resp.json() as ViewersFinancialDashboardResponse;
 
-		// @ts-ignore
-		if (acc && acc.familyBalances && acc.familyBalances.length > 0) {
-			// @ts-ignore
-			return acc.familyBalances[0].balance.amount;
+		if (acc?.familyBalances && acc?.familyBalances.length > 0) {
+			return acc.familyBalances[0].balance?.amount || 0.0;
 		}
+
+		console.log(`[getBalance] Response status: ${resp.status}`);
+		console.log(`[getBalance] Response headers: ${JSON.stringify(resp.headers)}`);
+		console.log(`[getBalance] Response body: ${JSON.stringify(acc)}`);
 
 		return 0.0;
 	}
 
-	private async getAuth(): Promise<BBVAAuth> {
-		if (!!this.accountReady) {
-			return await this.accountReady;
-		}
+	private async getAuth(): Promise<BBVAAuth | undefined> {
+		const postBody = {
+			authentication: {
+				consumerID: "00000001",
+				authenticationType: "02",
+				userID: `0019-0${this.user}`,
+				authenticationData: [
+					{ authenticationData: [this.password], idAuthenticationData: "password" },
+				],
+			},
+		};
 
-		this.accountReady = new Promise<BBVAAuth>(async (resolve) => {
-			const postBody = {
-				authentication: {
-					consumerID: "00000001",
-					authenticationType: "02",
-					userID: `0019-0${this.user}`,
-					authenticationData: [
-						{ authenticationData: [this.password], idAuthenticationData: "password" },
-					],
+		const resp = await fetch(
+			"https://www.bbva.es/ASO/TechArchitecture/grantingTickets/V02",
+			{
+				headers: {
+					"content-type": "application/json",
 				},
-			};
+				body: JSON.stringify(postBody),
+				method: "POST",
+			}
+		);
 
-			const resp = await fetch(
-				"https://www.bbva.es/ASO/TechArchitecture/grantingTickets/V02",
-				{
-					headers: {
-						"content-type": "application/json",
-					},
-					body: JSON.stringify(postBody),
-					method: "POST",
-				}
-			);
+		const authToken = resp.headers.get("tsec");
+		const json = await resp.json() as GrantingTicketsResponse;
 
-			const authToken = resp.headers.get("tsec");
-			const json = await resp.json();
+		console.log(`[getAuth] Response status: ${resp.status}`);
+		console.log(`[getAuth] Response headers: ${JSON.stringify(resp.headers)}`);
+		console.log(`[getAuth] Response body: ${JSON.stringify(json)}`);
 
-			// @ts-ignore
-			const customerId = json.user.id;
+		const customerId = json.user?.id || "";
 
-			resolve({ authToken: authToken!, customerId });
-		});
-
-		return await this.accountReady;
+		if (customerId && authToken) {
+			return { authToken, customerId };
+		}
 	};
 }
